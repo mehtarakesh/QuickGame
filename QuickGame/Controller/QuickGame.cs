3c95ea6fbda340a7759863ea9d59c20a1c472d3c
tristangaebler <Tristan.Gaebler@gmail.com>
2016-05-26 09:08:08 -0600

    Added the enemies

--- QuickGame/Controller/QuickGame.cs
+++ QuickGame/Controller/QuickGame.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
+using System.Collections.Generic;
 using QuickGame.Model;
 using QuickGame.View;
 
@@ -15,6 +16,16 @@ namespace QuickGame.Controller
 	/// </summary>
 	public class QuickGame : Game
 	{
+		// Enemies
+		Texture2D enemyTexture;
+		List<Enemy> enemies;
+
+		// The rate at which the enemies appear
+		TimeSpan enemySpawnTime;
+		TimeSpan previousSpawnTime;
+
+		// A random number generator
+		Random random;
 		// Image used to display the static background
 		Texture2D mainBackground;
 
@@ -58,6 +69,17 @@ namespace QuickGame.Controller
 			bgLayer1 = new Parallaxingbackground();
 			bgLayer2 = new Parallaxingbackground();
 
+			// Initialize the enemies list
+			enemies = new List<Enemy> ();
+
+			// Set the time keepers to zero
+			previousSpawnTime = TimeSpan.Zero;
+
+			// Used to determine how fast enemy respawns
+			enemySpawnTime = TimeSpan.FromSeconds(1.0f);
+
+			// Initialize our random number generator
+			random = new Random();
 			// Set a constant player move speed
 			playerMoveSpeed = 8.0f;
 
@@ -88,8 +110,9 @@ namespace QuickGame.Controller
 			player.Initialize(playerAnimation, playerPosition);
 
 			// Load the parallaxing background
-			bgLayer1.Initialize(Content, "bgLayer1", GraphicsDevice.Viewport.Width, -1);
-			bgLayer2.Initialize(Content, "bgLayer2", GraphicsDevice.Viewport.Width, -2);
+			bgLayer1.Initialize(Content, "Texture/bgLayer1", GraphicsDevice.Viewport.Width, -1);
+			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
+			enemyTexture = Content.Load<Texture2D>("Animation/mineAnimation");
 
 			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
 			//TODO: use this.Content to load your game content here 
@@ -123,6 +146,7 @@ namespace QuickGame.Controller
 			UpdatePlayer(gameTime);
 			bgLayer1.Update();
 			bgLayer2.Update();
+			UpdateEnemies(gameTime);
             
 			base.Update (gameTime);
 		}
@@ -178,6 +202,12 @@ namespace QuickGame.Controller
 			// Draw the moving background
 			bgLayer1.Draw(spriteBatch);
 			bgLayer2.Draw(spriteBatch);
+
+			// Draw the Enemies
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				enemies[i].Draw(spriteBatch);
+			}
 			// Draw the Player
 			player.Draw(spriteBatch);
 
@@ -187,6 +217,50 @@ namespace QuickGame.Controller
 			//TODO: Add your drawing code here
             
 			base.Draw (gameTime);
+		}
+
+		private void AddEnemy()
+		{ 
+			// Create the animation object
+			Animation enemyAnimation = new Animation();
+
+			// Initialize the animation with the correct animation information
+			enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 47, 61, 8, 30,Color.White, 1f, true);
+
+			// Randomly generate the position of the enemy
+			Vector2 position = new Vector2(GraphicsDevice.Viewport.Width +enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height -100));
+
+			// Create an enemy
+			Enemy enemy = new Enemy();
+
+			// Initialize the enemy
+			enemy.Initialize(enemyAnimation, position); 
+
+			// Add the enemy to the active enemies list
+			enemies.Add(enemy);
+		}
+
+		private void UpdateEnemies(GameTime gameTime)
+		{
+			// Spawn a new enemy enemy every 1.5 seconds
+			if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime) 
+			{
+				previousSpawnTime = gameTime.TotalGameTime;
+
+				// Add an Enemy
+				AddEnemy();
+			}
+
+			// Update the Enemies
+			for (int i = enemies.Count - 1; i >= 0; i--) 
+			{
+				enemies[i].Update(gameTime);
+
+				if (enemies[i].Active == false)
+				{
+					enemies.RemoveAt(i);
+				} 
+			}
 		}
 	}
 }
