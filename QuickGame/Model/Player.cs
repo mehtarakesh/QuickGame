e5b40399b63f04a1e84c161d3170a35e83b20b96
tristangaebler <Tristan.Gaebler@gmail.com>
2016-05-26 08:24:33 -0600

    Added the player class

---
+++ QuickGame/Model/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shooter
+{
+	class Player
+	{
+		public void Initialize()
+		{
+		}
+
+		public void Update()
+		{
+		}
+
+		public void Draw()
+		{
+		}
+	}
+}
