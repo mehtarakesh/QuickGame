a83ce1b3829b1f1d570fa3c442f31f7cf3fd0d2f
tristangaebler <Tristan.Gaebler@gmail.com>
2016-05-26 08:35:34 -0600

    Added the update player method

--- QuickGame/Controller/QuickGame.cs
+++ QuickGame/Controller/QuickGame.cs
@@ -4,6 +4,8 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using QuickGame.Model;
 
 namespace QuickGame.Controller
 {
@@ -14,6 +16,20 @@ namespace QuickGame.Controller
 	{
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
+		// Represents the player 
+
+		Player player;
+
+		// Keyboard states used to determine key presses
+		KeyboardState currentKeyboardState;
+		KeyboardState previousKeyboardState;
+
+		// Gamepad states used to determine button presses
+		GamePadState currentGamePadState;
+		GamePadState previousGamePadState; 
+
+		// A movement speed for the player
+		float playerMoveSpeed;
 
 		public QuickGame ()
 		{
@@ -29,6 +45,14 @@ namespace QuickGame.Controller
 		/// </summary>
 		protected override void Initialize ()
 		{
+			player = new Player();
+
+
+			// Set a constant player move speed
+			playerMoveSpeed = 8.0f;
+
+			//Enable the FreeDrag gesture.
+			TouchPanel.EnabledGestures = GestureType.FreeDrag;
 			// TODO: Add your initialization logic here
             
 			base.Initialize ();
@@ -42,7 +66,9 @@ namespace QuickGame.Controller
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch (GraphicsDevice);
-
+			// Load the player resources 
+			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X,GraphicsDevice.Viewport.TitleSafeArea.Y +GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			player.Initialize(Content.Load<Texture2D>("player"), playerPosition);
 			//TODO: use this.Content to load your game content here 
 		}
 
@@ -65,6 +91,40 @@ namespace QuickGame.Controller
 			base.Update (gameTime);
 		}
 
+		private void UpdatePlayer(GameTime gameTime)
+		{
+
+			// Get Thumbstick Controls
+			player.Position.X += currentGamePadState.ThumbSticks.Left.X *playerMoveSpeed;
+			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y *playerMoveSpeed;
+
+			// Use the Keyboard / Dpad
+			if (currentKeyboardState.IsKeyDown(Keys.Left) ||
+				currentGamePadState.DPad.Left == ButtonState.Pressed)
+			{
+				player.Position.X -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Right) ||
+				currentGamePadState.DPad.Right == ButtonState.Pressed)
+			{
+				player.Position.X += playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Up) ||
+				currentGamePadState.DPad.Up == ButtonState.Pressed)
+			{
+				player.Position.Y -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Down) ||
+				currentGamePadState.DPad.Down == ButtonState.Pressed)
+			{
+				player.Position.Y += playerMoveSpeed;
+			}
+
+			// Make sure that the player does not go out of bounds
+			player.Position.X = MathHelper.Clamp(player.Position.X, 0,GraphicsDevice.Viewport.Width - player.Width);
+			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0,GraphicsDevice.Viewport.Height - player.Height);
+		}
+
 		/// <summary>
 		/// This is called when the game should draw itself.
 		/// </summary>
@@ -72,6 +132,15 @@ namespace QuickGame.Controller
 		protected override void Draw (GameTime gameTime)
 		{
 			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+
+			// Start drawing
+			spriteBatch.Begin();
+
+			// Draw the Player
+			player.Draw(spriteBatch);
+
+			// Stop drawing
+			spriteBatch.End();
             
 			//TODO: Add your drawing code here
             
--- QuickGame/Model/Player.cs
+++ QuickGame/Model/Player.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Shooter
+namespace QuickGame.Model
 {
 	class Player
 	{
