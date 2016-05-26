c3e203a832b214369b0d11cf720731b2397338dc
tristangaebler <Tristan.Gaebler@gmail.com>
2016-05-26 08:47:58 -0600

    Updated the game animation

--- QuickGame/Controller/QuickGame.cs
+++ QuickGame/Controller/QuickGame.cs
@@ -67,8 +67,15 @@ namespace QuickGame.Controller
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch (GraphicsDevice);
 			// Load the player resources 
-			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X,GraphicsDevice.Viewport.TitleSafeArea.Y +GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
-			player.Initialize(Content.Load<Texture2D>("Texture/player"), playerPosition);
+			//Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X,GraphicsDevice.Viewport.TitleSafeArea.Y +GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			// Load the player resources
+			Animation playerAnimation = new Animation();
+			Texture2D playerTexture = Content.Load<Texture2D>("Animation/shipAnimation");
+			playerAnimation.Initialize(playerTexture, Vector2.Zero, 115, 69, 8, 30, Color.White, 1f, true);
+
+			Vector2 playerPosition = new Vector2 (GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y
+				+ GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			player.Initialize(playerAnimation, playerPosition);
 			//TODO: use this.Content to load your game content here 
 		}
 
@@ -103,6 +110,7 @@ namespace QuickGame.Controller
 
 		private void UpdatePlayer(GameTime gameTime)
 		{
+			player.Update(gameTime);
 
 			// Get Thumbstick Controls
 			player.Position.X += currentGamePadState.ThumbSticks.Left.X *playerMoveSpeed;
